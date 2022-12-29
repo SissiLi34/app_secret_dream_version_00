0d9f504bf5766fd3aff71e5ff566a44f0d11fd41
SissiLi34 <92378904+SissiLi34@users.noreply.github.com>
2022-12-29 18:03:01 +0100

    1er commit

---
+++ app/src/main/java/fr/sissi/app_secret_dream_version_3/MainActivity.kt
@@ -0,0 +1,11 @@
+package fr.sissi.app_secret_dream_version_3
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
